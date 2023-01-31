60145caf243873753f8d4a042e1ec747484cc6ea
JoldoshAjimudunov <joldoshbek8856@gmail.com>
2023-01-31 11:52:17 +0600

    features

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Carousel1 from "./components/carousel/Carousel1";
+import Footer from "./components/footer/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Carousel1 />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
