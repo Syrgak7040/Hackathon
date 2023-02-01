93959d52f614b681739337bf54f7613ac2be711f
JoldoshAjimudunov <joldoshbek8856@gmail.com>
2023-02-01 12:22:42 +0600

    newver

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
+import Bottom from "./components/bottom/Bottom";
 import Carousel1 from "./components/carousel/Carousel1";
 import Footer from "./components/footer/Footer";
+// import Cart from "./components/shop/Cart";
 
 function App() {
   return (
     <div className="App">
+      {/* <Cart /> */}
       <Carousel1 />
+      <Bottom />
       <Footer />
     </div>
   );
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -18,9 +18,7 @@ const Footer = () => {
 
   const aboutus = {
     color: "gray",
-    mt: "10px",
-    ml: "30px",
-    mr: "20px",
+    m: "10px  30px 20px 20px",
   };
   const text__default = {
     color: "white",
@@ -36,104 +34,12 @@ const Footer = () => {
     mt: "10px",
     mb: "30px",
   };
+
   return (
     <div>
-      <div
-        className={cl.box}
-        sx={{ width: "1550px" }}
-        style={{
-          backgroundImage:
-            "url(https://www.centerformentalhealth.in/wp-content/uploads/2021/01/beautiful-scenery-rocky-mountains-with-green-landscape-cloudy-sky-1.jpg)",
-        }}
-      >
-        <div className={cl.text__back}></div>
-        <Typography sx={{ width: "600px", pt: "40px", pb: "40px" }}>
-          Мы не просто продаем одежду и снаряжение для активных видов спорта на
-          природе, наша команда влюблена в горы. Мы знаем о них все и
-          профессионально занимаемся многими видами горного спорта.
-          Сотрудничество с ведущими спортивными брендами, участие в
-          международных выставках, экспедициях, тестирование снаряжения – это
-          опыт и знания, которыми мы гордимся и с удовольствием делимся.
-          Опираясь на них, нами сделан выбор в пользу определенных известных
-          мировых брендов, официальным дистрибьютором которых «Sport Expert»
-          является более 13 лет. Учитывая Ваши пожелания и наш опыт – покупка в
-          нашей компании будет не только приятной, но и полезной. Выбирая
-          высокотехнологичную одежду, экипировку для горных лыж или сноуборда,
-          велосипед, палатку, спальный мешок, рюкзак или снаряжение для
-          альпинизма вы начнете свое увлекательное путешествие уже с порога
-          нашего магазина. Спорт Эксперт — это не только снаряжение, но и
-          единомышленники у которых можно получить Экспертный совет. Наш магазин
-          — это не просто красивая выкладка товара в четырех стенах!
-        </Typography>
-      </div>
-      <div className={cl.insta__header}>
-        <div className={cl.inner__insta}>
-          <h2>INSTAGRAM</h2>
-          <hr />
-        </div>
-        <div>
-          <a href="https://www.instagram.com/reebok/" target="_blank">
-            <Typography variant="h5" component="h2">
-              @reebok
-            </Typography>
-          </a>
-          <div>
-            <Grid container spacing={3}>
-              <a
-                href="https://www.instagram.com/p/Cjiien5LF0R/"
-                target="_blank"
-              >
-                <img
-                  className={cl.reebok}
-                  src="https://cdn.zando.co.za/p/410058-4061-850014-1-zoom.jpg"
-                  alt=""
-                />
-              </a>
-              <a
-                href="https://www.instagram.com/p/CkHJdjTJpA0/"
-                target="_blank"
-              >
-                <img
-                  className={cl.reebok}
-                  src="https://z2photorankmedia-a.akamaihd.net/media/c/5/p/c5pqay4/normal.jpg"
-                  alt=""
-                />
-              </a>
-              <a
-                href="https://www.instagram.com/p/CkgyYEGP29m/"
-                target="_blank"
-              >
-                <img
-                  className={cl.reebok}
-                  src="https://www.grandinettisport.com/pub/media/catalog/product/cache/ce839fe888c38210e51d91bbee82a72d/r/e/reebok-pump-omni-lite-m41447.jpg"
-                  alt=""
-                />
-              </a>
-              <a
-                href="https://www.instagram.com/p/Ck_sD7yuDSB/"
-                target="_blank"
-              >
-                <img
-                  className={cl.reebok}
-                  src="https://images.dsw.com/is/image/DSWShoes/P221401_header_reebok-mobile?impolicy=qlt-medium&imwidth=768&imdensity=1"
-                  alt=""
-                />
-              </a>
-            </Grid>
-          </div>
-        </div>
-      </div>
-
-      <Box
-        sx={{
-          width: 1550,
-          height: 600,
-          backgroundColor: "rgb(40, 40, 40)",
-          mt: "100px",
-        }}
-      >
-        <Grid container spacing={3}>
-          <Grid xs>
+      <div className={cl.footer}>
+        <Grid container spacing={3} columns={{ xs: 1, sm: 2, md: 3 }}>
+          <Grid item xs>
             <List
               sx={(style, { ml: "40px" })}
               component="nav"
@@ -161,7 +67,7 @@ const Footer = () => {
               </Typography>
             </List>
           </Grid>
-          <Grid xs={-1}>
+          <Grid item xs={0}>
             <List sx={style} component="nav" aria-label="mailbox folders">
               <Typography variant="h4" component="h2" sx={text__default}>
                 контакты:
@@ -184,7 +90,7 @@ const Footer = () => {
               </div>
             </List>
           </Grid>
-          <Grid xs>
+          <Grid item xs>
             <Typography variant="h4" component="h2" sx={text__default}>
               мы на карте:
             </Typography>
@@ -208,7 +114,7 @@ const Footer = () => {
             </div>
           </Grid>
         </Grid>
-      </Box>
+      </div>
     </div>
   );
 };
